fd77ff4e03c867e4cc4d7f4b5310f607a022570f
Md Fuad Amir <mdfuadamir@gmail.com>
2024-12-02 21:51:47 +0600

    added github login

--- src/Components/GithubLogin/GithubLogin.jsx
+++ src/Components/GithubLogin/GithubLogin.jsx
@@ -1,7 +1,47 @@
+import { GithubAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { useState } from "react";
+import auth from "../../Firebase/firebase.init";
 
 const GithubLogin = () => {
+    const [user,setUser] = useState(null);
+    const githubProvider = new GithubAuthProvider();
+    const handleGithubLogin = () =>{
+        signInWithPopup(auth,githubProvider)
+        .then((result) =>{
+            const githubLoggedin = result.user;
+            setUser(githubLoggedin)
+            console.log(githubLoggedin);
+
+        })
+        .catch((error) =>{
+            console.error(error);
+        })
+    }
+    const handleGithubSignOut = () =>{
+        signOut(auth)
+        .then((result) =>{
+            setUser(null);
+            alert(result,"Github SignOut successfully");
+        })
+        .catch((error)=>{
+            console.error(error);
+        })
+
+    }
     return (
-        <div>
+        <div className="h-screen flex flex-col justify-center items-center">
+            <div>
+            { user &&
+                <div className="border-2 rounded-md p-4 mb-6 space-y-3">
+                <h1>Name: {user?.displayName}</h1>
+                <h1>Email:{user?.email}</h1>
+                </div>
+            }
+
+            </div>
+            {
+                user ? <button onClick={handleGithubSignOut} className="btn">Github Sign Out</button> : <button onClick={handleGithubLogin} className="btn">Github Login</button>
+            }
             
         </div>
     );
